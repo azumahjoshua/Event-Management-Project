f59c292180543f0eab80c99a6204f56ebad90fe6
azumahjoshua <joshuaazumah1086@gmail.com>
2023-01-28 20:25:11 +0100

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# studentportal
+# Event Management Project in PHP
+
+This project is a web-based event management system that allows logged-in users to add, delete, and update events.
+The client-side form validation is done using JavaScript.
+
+### Features
+
+- User login and registration system
+- Add events with details such as name, location, date, and time
+- Delete and update existing events
+- Client-side form validation using JavaScript
+- User can only manage the events added by them
+
+### Requirements
+
+- PHP 7.x or higher
+- MySQL 5.x or higher
+- JavaScript
+
+### Usage
+
+1. Register as a new user or log in if you already have an account
+2. Once logged in, you will be able to add, delete, and update events
+3. The client-side form validation will ensure that all required fields are filled before submitting the form
+4. Only the events added by the logged-in user can be managed
+
+### Support
+
+If you have any issues or questions regarding the project, please contact the developer or open an issue on the repository.
+
+Email: joshuaazumah6801@gmail.com <br/>
+Linkedin: https://www.linkedin.com/in/azumahjoshua
--- about.php
+++ about.php
@@ -7,25 +7,25 @@ include_once './header.php'
             <img src="./images/lina2.png" alt="lina">
         </div>
         <div class="profile_text">
-            <h3 class="headering">
-                Hello!!! I'm Lina Saeed Yakubut
-            </h3>
-            <p>I'm a first year student Software Enginering student in Morocoo.</p>
-            <p>She is very passionate about technology and that what drove her to persue Software Enginering </p>
-            <p>She has experince in several  programming languages including JavaScript,php,python</p>
+            <!-- <h3 class="headering"> -->
+                <!-- Hello!!! I'm Lina Saeed Yakubut -->
+            <!-- </h3> -->
+            <!-- <p>I'm a first year student Software Enginering student in Morocoo.</p> -->
+            <!-- <p>She is very passionate about technology and that what drove her to persue Software Enginering </p> -->
+            <!-- <p>She has experince in several  programming languages including JavaScript,php,python</p> -->
         </div>
     </div>
 </section>
 <section class="about-section-website">
     <div class="about-website-container">
-        <h3>
-            SIST Student Union website
-        </h3>
-        <p>The SIST Student Union website is a  student event management. 
-            It provides login users with easy and unrestricted access to create, edit, delete, and manage events as they see fit. 
-            The website has been meticulously designed to provide a smooth and user-friendly experience that meets the needs of all users without any problems. 
-            The website's frontend is built with HTML, CSS, and JavaScript, while the backend is built with PHP, making it fast and dependable. 
-            This website has everything you need to make your events a success, whether you're an organizer or a participant.</p>
+        <!-- <h3> -->
+            <!-- SIST Student Union website -->
+        <!-- </h3> -->
+        <!-- <p>The SIST Student Union website is a  student event management.  -->
+            <!-- It provides login users with easy and unrestricted access to create, edit, delete, and manage events as they see fit.  -->
+            <!-- The website has been meticulously designed to provide a smooth and user-friendly experience that meets the needs of all users without any problems.  -->
+            <!-- The website's frontend is built with HTML, CSS, and JavaScript, while the backend is built with PHP, making it fast and dependable.  -->
+            <!-- This website has everything you need to make your events a success, whether you're an organizer or a participant.</p> -->
     </div>
 </section>
 <?php
